82c63c8b3c01785162ba8725f79a0d0a2063393f
markmwaura <markkmwaura@gmail.com>
2016-07-11 08:48:07 +0300

    leapyear assignment

---
+++ src/MainLeap.java
@@ -0,0 +1,23 @@
+/**
+ * Created by mark on 11/07/16.
+ */
+public class MainLeap {
+
+    public static void main(String args[]){
+
+        for(int thisyear = 2016; thisyear<2036;thisyear++){
+
+
+            if(thisyear%4 ==0){
+                System.out.println(thisyear);
+
+
+            }
+
+
+
+        }
+
+
+    }
+}
